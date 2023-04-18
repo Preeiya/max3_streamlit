d50a691858b76483056b0497904081a78c127862
Preeiya <krishnapriya.nkannan@gmail.com>
2023-04-19 01:55:47 +0530

    Update tds_ga8.py

--- tds_ga8.py
+++ tds_ga8.py
@@ -11,9 +11,9 @@ def find_largest(x, y, z):
 st.title('Find the largest among 3 numbers')
 
 num1 = st.number_input('Enter the first number',format='%d')
-num2 = st.number_input('Enter the second number')
-num3 = st.number_input('Enter the third number')
+num2 = st.number_input('Enter the second number',format='%d')
+num3 = st.number_input('Enter the third number',format='%d')
 
 if st.button('Find Largest'):
     largest =find_largest(num1, num2, num3)
-    st.write(f'The largest number is: {largest}')
+    st.write(f'The largest number is: {largest}',format='%d')
