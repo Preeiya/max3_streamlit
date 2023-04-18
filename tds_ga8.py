027a10b143a87054fe1670d8f51d3d22ab862a49
Preeiya <krishnapriya.nkannan@gmail.com>
2023-04-19 00:41:40 +0530

    Update tds_ga8.py

--- tds_ga8.py
+++ tds_ga8.py
@@ -8,12 +8,9 @@ Original file is located at
 """
 
 pip install streamlit
-pip install sklearn
 
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
 import pickle
 
 st.write("""
